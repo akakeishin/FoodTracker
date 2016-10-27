e60944dfbfe1bb81eb47c2cfad2eef2ba894b0fa
akakeishin <mizururi_22@gmail.com>
2016-10-28 01:56:19 +0900

    add 5 buttons ボタンがいっぱいっだぞー

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class RatingControl: UIView {
+    
+    // MARK: Properties
+    var rating = 0
+    var ratingButtons = [UIButton]()
+    
+    let spacing = 5
+    let starCount = 5
+    
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-        button.backgroundColor = UIColor.blue
-        addSubview(button)
+//        let buttonSize = Int(frame.size.height)
+        for _ in 0..<starCount {
+            let button = UIButton()
+            button.backgroundColor = UIColor.blue
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
+    }
+    
+    override func layoutSubviews() {
+        // Set the button's width and height to a square the size of frame's height
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        
+        // Offset each button's origin by the length of the button plus spacing
+        for (index, button) in ratingButtons.enumerated() {
+            buttonFrame.origin.x = CGFloat(index * (buttonSize+spacing))
+            button.frame = buttonFrame
+        }
     }
 
     override var intrinsicContentSize: CGSize {
-        return CGSize(width: 240, height: 44)
+        let buttonSize = Int(frame.size.height)
+        let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        
+        return CGSize(width: width, height: buttonSize)
+    }
+    
+    // MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("button Tapped!")
     }
 
 }
